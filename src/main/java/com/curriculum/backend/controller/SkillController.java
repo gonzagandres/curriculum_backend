a9038a098c425c340dad7b155f5854db8098bf31
gonzagandres2002 <114045995+gonzagandres@users.noreply.github.com>
2024-06-10 23:21:44 -0500

    cross Origins modified

--- src/main/java/com/curriculum/backend/controller/SkillController.java
+++ src/main/java/com/curriculum/backend/controller/SkillController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://master--curriculum-vitae-udea.netlify.app/")
 @RestController
 @RequestMapping("/api/skills")
 public class SkillController {
